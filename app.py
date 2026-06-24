64b8065536de9afbd3199622cfa57b885cc47cd3
Sonia Nanwani <soniananvani7@gmail.com>
2026-06-24 20:45:37 +0530

    Added first jinja template

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask , render_template
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Welcome to ResearchHub</h1>
-    <p> it is a research platform</p>
-    """
+    return render_template("home.html")
 
 if __name__=="__main__":
     app.run(debug=True)
