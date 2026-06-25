5f6de23c7dccb6ff299943933107aad9dc89ea95
Sonia Nanwani <soniananvani7@gmail.com>
2026-06-25 14:11:05 +0530

    Implemented registration and login forms

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask , render_template
+from flask import (Flask,render_template,request,redirect,url_for)
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    name="Sonia"
+    return render_template("home.html",name=name)
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+
+        name=request.form["name"]
+        email=request.form["email"]
+
+        print(name)
+        print(email)
+
+        return redirect(url_for("login"))
+    
+    return render_template("register.html")
+
+@app.route("/login",methods=["GET,POST"])
+def login():
+    if request.method=="POST":
+        email=request.form["email"]
+        print(email)
+
+        return redirect(url_for("home"))
+    return render_template("login.html")
 
 if __name__=="__main__":
     app.run(debug=True)
